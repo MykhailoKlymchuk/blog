478a8a05d462abed130d112fa2968a5c78794309
KMS <klimchukmikhail57@gmail.com>
2023-05-05 19:22:29 +0300

    update

--- src/main/java/com/blog/api/BlogApplication.java
+++ src/main/java/com/blog/api/BlogApplication.java
@@ -1,15 +1,10 @@
 package com.blog.api;
 
-import com.blog.api.entity.Role;
-import com.blog.api.repository.RoleRepository;
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.info.License;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -27,8 +22,7 @@ import org.springframework.context.annotation.Bean;
         ),
         externalDocs = @ExternalDocumentation(
                 description = "Spring Boot Blog App Rest APIs documentation",
-                //todo add github link
-                url = "github"
+                url = "https://github.com/MykhailoKlymchuk/blog"
         )
 )
 public class BlogApplication {
